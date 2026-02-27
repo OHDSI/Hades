cc31964ca5fce283aee08d264555c37306b65185
schuemie <schuemie@ohdsi.org>
2026-02-27 11:58:28 +0100

    Updating .lintr file. Adding to code style page

--- R/Packages.R
+++ R/Packages.R
@@ -1,13 +1,13 @@
 # Copyright 2025 Observational Health Data Sciences and Informatics
 #
 # This file is part of Hades
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,7 +21,7 @@
 #'
 #' @examples
 #' listHadesPackages()
-#' 
+#'
 #' @export
 listHadesPackages <- function() {
   packageListUrl <- "https://raw.githubusercontent.com/OHDSI/Hades/main/extras/packages.csv"
